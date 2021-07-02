1404b7b74002a55e9709283fe420cdd7221a98aa
Tatsuya Sato <satoryu.1981@gmail.com>
2021-07-02 15:19:04 +0900

    Refactoring: extract functions

--- popup.js
+++ popup.js
@@ -2,29 +2,45 @@ let messageBox = document.getElementById('message-box');
 let copyCurrentTabButton = document.getElementById('copy-current-tab-button');
 let copySelectedTabsButton = document.getElementById('copy-selected-tabs-button');
 
-copyCurrentTabButton.addEventListener('click', () => {
-  chrome.tabs.query({ active: true, currentWindow: true })
-    .then(([tab]) => {
-      let linkText = `[${tab.url} ${tab.title}]`;
+function createLinksForTabs(tabs) {
+  return tabs.map(tab => ` [${tab.url} ${tab.title}]`).join("\n")
+}
 
-      navigator.clipboard.writeText(linkText).then(() => {
+function writeTextToClipboard(text) {
+  return navigator.clipboard.writeText(text)
+}
+
+function appendMessage(messageElement) {
+  messageBox.appendChild(messageElement)
+}
+
+function findTabs(queryOption) {
+  return chrome.tabs.query(queryOption)
+}
+
+copyCurrentTabButton.addEventListener('click', () => {
+  findTabs({ active: true, currentWindow: true })
+    .then(([tab]) => {
+      let linkText = createLinksForTabs([tab])
+
+      writeTextToClipboard(linkText).then(() => {
         let element = document.createElement('p')
         element.innerText = 'Copied!'
-        messageBox.appendChild(element)
+        appendMessage(element)
       })
     })
     .catch((err) => console.error(err))
 });
 
 copySelectedTabsButton.addEventListener('click', () => {
-  chrome.tabs.query({ highlighted: true, currentWindow: true })
+  findTabs({ highlighted: true, currentWindow: true })
     .then(tabs => {
-      let linkText = tabs.map(tab => ` [${tab.url} ${tab.title}]`).join("\n")
+      let linkText = createLinksForTabs(tabs)
 
-      navigator.clipboard.writeText(linkText).then(() => {
+      writeTextToClipboard(linkText).then(() => {
         let element = document.createElement('p')
         element.innerText = 'Copied Selected Tabs!'
-        messageBox.appendChild(element)
+        appendMessage(element)
       })
     })
     .catch((err) => console.error(err))
