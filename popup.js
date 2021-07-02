515f51d1ae5121015b369b912422c61bd0185486
Tatsuya Sato <satoryu.1981@gmail.com>
2021-07-02 13:56:37 +0900

    Add a button to make links for selected tabs

--- popup.js
+++ popup.js
@@ -1,7 +1,8 @@
 let messageBox = document.getElementById('message-box');
-let copyButton = document.getElementById('copy-button');
+let copyCurrentTabButton = document.getElementById('copy-current-tab-button');
+let copySelectedTabsButton = document.getElementById('copy-selected-tabs-button');
 
-copyButton.addEventListener('click', () => {
+copyCurrentTabButton.addEventListener('click', () => {
   chrome.tabs.query({ active: true, currentWindow: true })
     .then(([tab]) => {
       let linkText = `[${tab.url} ${tab.title}]`;
@@ -14,3 +15,17 @@ copyButton.addEventListener('click', () => {
     })
     .catch((err) => console.error(err))
 });
+
+copySelectedTabsButton.addEventListener('click', () => {
+  chrome.tabs.query({ highlighted: true, currentWindow: true })
+    .then(tabs => {
+      let linkText = tabs.map(tab => ` [${tab.url} ${tab.title}]`).join("\n")
+
+      navigator.clipboard.writeText(linkText).then(() => {
+        let element = document.createElement('p')
+        element.innerText = 'Copied Selected Tabs!'
+        messageBox.appendChild(element)
+      })
+    })
+    .catch((err) => console.error(err))
+})
