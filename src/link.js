7d2ac1df6f91a4d9ab25628f85e006810ee1861a
Tatsuya Sato <satoryu.1981@gmail.com>
2021-09-15 01:00:49 +0900

    Trim back quotes

--- src/link.js
+++ src/link.js
@@ -1,6 +1,6 @@
 function createLinksForTabs(tabs) {
   return tabs.map(tab => {
-    let title = tab.title.replaceAll(/[\[\]]/g, '')
+    let title = tab.title.replaceAll(/[\[\]]/g, '').replaceAll(/`(.*)`/g, '$1')
 
     return ` [${tab.url} ${title}]`
   }).join("\n")
