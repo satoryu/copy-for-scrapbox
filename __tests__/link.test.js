04597b52588937b5cac922654f4b5844d01d1cbc
Tatsuya Sato <satoryu.1981@gmail.com>
2022-11-16 01:41:46 +0900

    Adds tests for link.js

---
+++ __tests__/link.test.js
@@ -0,0 +1,13 @@
+import { createLinkForTab } from '../src/link.js'
+
+test("creates a link from a given tab's title and url", () => {
+  const link = createLinkForTab({ title: 'title', url: 'https://example.com'})
+
+  expect(link).toEqual(" [https://example.com title]")
+})
+
+test("omits square brackets in title", () => {
+  const link = createLinkForTab({ title: '[title]', url: 'https://example.com' })
+
+  expect(link).toEqual(" [https://example.com title]")
+})
