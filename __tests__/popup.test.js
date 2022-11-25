91ce71c1ba2909136114da23fed539d49c41ea8b
Tatsuya Sato <satoryu.1981@gmail.com>
2022-11-25 11:30:05 +0900

    Add a test for checking copying selected tabs

--- __tests__/popup.test.js
+++ __tests__/popup.test.js
@@ -2,6 +2,17 @@ import { screen, waitFor } from '@testing-library/dom'
 import userEvent from '@testing-library/user-event'
 
 describe("appendMessage", () => {
+  beforeAll(() => {
+    document.body.innerHTML = `
+      <div id="message-box"></div>
+
+      <button id="copy-current-tab-button">Copy Current Tab</button>
+      <button id="copy-selected-tabs-button">Copy Selected Tabs</button>
+    `;
+
+    require("./../popup.js");
+  })
+
   test("insert p tag to show a given message", async () => {
     jest.mock("../src/clipboard", () => {
       return {
@@ -20,14 +31,6 @@ describe("appendMessage", () => {
       };
     });
 
-    document.body.innerHTML = `
-      <div id="message-box"></div>
-
-      <button id="copy-current-tab-button">Copy Current Tab</button>
-      <button id="copy-selected-tabs-button">Copy Selected Tabs</button>
-    `;
-
-    require("./../popup.js");
 
     const user = userEvent.setup()
     const button = screen.getByRole('button', { name: 'Copy Current Tab' })
@@ -37,4 +40,34 @@ describe("appendMessage", () => {
     const message = await screen.getByText('Copied')
     expect(message).toBeTruthy()
   });
+
+  describe("When clicking copy selected tab button", () => {
+    jest.mock("../src/clipboard", () => {
+      return {
+        writeTextToClipboard: jest.fn(() => {
+          return Promise.resolve("hoge");
+        }),
+      };
+    });
+    jest.mock("../src/chrome.js", () => {
+      return {
+        findTabs: jest.fn(() => {
+          return Promise.resolve([
+            { title: "hoge", url: "https://www.example.com" },
+            { title: "fuga", url: "https://fuga.example.com" },
+          ]);
+        }),
+      };
+    });
+
+    test("show the message", async () => {
+      const user = userEvent.setup()
+      const button = screen.getByRole('button', { name: 'Copy Selected Tabs' })
+      await user.click(button)
+
+      const message = await screen.getByText('Copied Selected Tabs!')
+      expect(message).toBeTruthy()
+    })
+
+  })
 });
