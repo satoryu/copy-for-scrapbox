53ee1a90833f5839ec86a6068347121d149b990d
Tatsuya Sato <satoryu.1981@gmail.com>
2023-07-17 23:11:25 +0900

    Add Google Analytics tracking and update tests

--- src/background.js
+++ src/background.js
@@ -1,9 +1,11 @@
 import { writeTextToClipboard } from './clipboard.js'
 import { createLinkForTab } from './link.js'
 import { getClientId } from './id.js'
+import { sendTrackEvent } from './google-analytics.js'
 
 chrome.runtime.onInstalled.addListener(function () {
   getClientId()
+  sendTrackEvent({ name: 'installed' })
   chrome.contextMenus.create({
     id: 'copy-for-scrapbox',
     title: 'Copy [URL PageTitle]'
--- src/google-analytics.js
+++ src/google-analytics.js
@@ -1,5 +1,20 @@
-async function sendTrackEvent() {
-  return "fake";
+import { getClientId } from "./id";
+
+const GA_ENDPOINT = process.env.VITE_GA_ENDPOINT;
+const MEASUREMENT_ID = process.env.VITE_MEASUREMENT_ID;
+const API_SECRET = process.env.VITE_API_SECRET;
+
+async function sendTrackEvent(event) {
+  return fetch(
+    `${GA_ENDPOINT}?measurement_id=${MEASUREMENT_ID}&api_secret=${API_SECRET}`,
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        client_id: await getClientId(),
+        events: [event]
+      })
+    }
+  );
 }
 
 export { sendTrackEvent };
