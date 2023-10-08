61d03a68f5e0f7bc8d4a8a0a8baa4149abe89f79
Tatsuya Sato <satoryu.1981@gmail.com>
2023-10-09 00:29:30 +0900

    Add 'copy as quotation' to context menu

--- src/background.js
+++ src/background.js
@@ -10,10 +10,29 @@ chrome.runtime.onInstalled.addListener(function () {
     id: 'copy-for-scrapbox',
     title: 'Copy [URL PageTitle]'
   })
+  chrome.contextMenus.create({
+    id: 'copy-selection-as-quotation',
+    contexts: ["selection"],
+    title: "Copy as Quotation"
+  })
 })
 
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
-  createLinkForTab(tab).then((text) => {
+  let contextMenuWork;
+  switch(info.menuItemId) {
+    case "copy-for-scrapbox":
+      contextMenuWork = createLinkForTab(tab)
+      break;
+    case "copy-selection-as-quotation":
+      contextMenuWork = createLinkForTab(tab)
+        .then((text) => (`> ${info.selectionText}\n> ${text}`))
+      break;
+    default:
+      console.warn(`Caught undefined menuItemId: ${info.menuItemId}`)
+      return;
+  }
+
+  contextMenuWork.then((text) => {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       func: writeTextToClipboard,
