8d0b1ab2fca7f183958d8cc2707864a371b5dacb
Tatsuya Sato <satoryu.1981@gmail.com>
2023-10-10 01:29:00 +0900

    Add context menu handler repository and tests

---
+++ src/context_menu/handler_repository.js
@@ -0,0 +1,17 @@
+export default class {
+  handlers = {}
+
+  registerHandler(handlerInfo) {
+    if (this.handlers[handlerInfo.id]) {
+      throw new Error('Handler already registered')
+    }
+
+    this.handlers[handlerInfo.id] = handlerInfo
+  }
+
+  async getHandler(menuId) {
+    const handler = this.handlers[menuId]?.handler
+
+    return handler
+  }
+}
