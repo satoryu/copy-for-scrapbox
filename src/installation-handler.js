314574c1f70e3643ffbcf5ca3636b29e845bdec4
Tatsuya Sato <satoryu.1981@gmail.com>
2024-09-16 16:50:54 +0900

    Refactoring

--- src/installation-handler.js
+++ src/installation-handler.js
@@ -1,22 +1,34 @@
 import { sendTrackEvent } from "./google-analytics"
 
+async function saveLastInstalledVersion() {
+  const { version } = chrome.runtime.getManifest()
+
+  return chrome.storage.local.set({ lastInstalledVersion: version })
+}
+
+async function getLastInstalledVersion() {
+  return chrome.storage.local.get('lastInstalledVersion')
+    .then(({ lastInstalledVersion }) => lastInstalledVersion)
+}
+
 export async function installationHandler({ previousVersion, reason }) {
   const { version } = chrome.runtime.getManifest()
 
   if (reason === 'install') {
     return sendTrackEvent({ name: 'installed', params: { version } })
-      .then(() => chrome.storage.local.set({ lastInstalledVersion: version }))
+      .then(saveLastInstalledVersion)
       .then(() => console.debug(`lastInstalledVersion is set: ${version}`))
   } else if (reason === 'update') {
-    chrome.storage.local.get('lastInstalledVersion')
-    .then(({ lastInstalledVersion }) => {
+    getLastInstalledVersion()
+    .then((lastInstalledVersion) => {
+      // if already installed, just update the version
       if (lastInstalledVersion) {
         console.debug(`lastInstalledVersion is ${lastInstalledVersion}`)
         return sendTrackEvent({ name: 'updated', params: { version, previousVersion } })
       }
 
       return sendTrackEvent({ name: 'installed', params: { version } })
-        .then(() => chrome.storage.local.set({ lastInstalledVersion: version }))
+        .then(saveLastInstalledVersion)
     })
   }
 }
