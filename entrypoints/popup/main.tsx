16087ef9048946f14fd7b5394798f60009c4e8a9
Tatsuya Sato <satoryu.1981@gmail.com>
2025-04-15 19:42:23 +0900

    Move App.tsx to entrypoints/popup directory

--- entrypoints/popup/main.tsx
+++ entrypoints/popup/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './components/App';
+import App from './App';
 import './popup.css';
 
 // Create root element
