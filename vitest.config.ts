e1fd5054acdf0756b5fabb919e2b11e4363f5aa0
Tatsuya Sato <satoryu.1981@gmail.com>
2025-04-15 16:32:13 +0900

    Migrate test codes

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vitest/config';
+import { WxtVitest } from 'wxt/testing';
+
+export default defineConfig({
+  plugins: [WxtVitest()],
+  test: {
+    globals: true,
+    watch: false,
+  }
+});
